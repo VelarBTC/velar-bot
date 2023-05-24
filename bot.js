adbb73f80d2ad0d43a2c9b03fa2cb2186ec8dba9
VelarBTC <dev@velar.co>
2023-05-24 22:12:51 +0530

    help button

--- bot.js
+++ bot.js
@@ -14,6 +14,10 @@ bot.onText(/\/start/, (msg) => {
           {
             text: 'FAQ',
             callback_data: 'faq'
+          },
+          {
+            text: 'HELP',
+            callback_data: 'help'
           }
         ]
       ]
@@ -32,5 +36,10 @@ bot.on('callback_query', (query) => {
     const faqMessage = 'Frequently Asked Questions:\n\nQuestion 1: What is Stacks(STX)\nAnswer 1: Stacks 2.0 is a blockchain that connects to Bitcoin and allows smart contract creation for dApps.\n\nQuestion2: What is Proof-of-Transfer (PoX)?\nAnswer 2: Proof-of-Transfer (PoX) is a novel category of consensus mechanism that transfers a base cryptocurrency to mint units of a new cryptocurrency. In the case of Stacks implementation of PoX, it is anchoring itself to Bitcoin as the underlying asset. The protocol works by Miners sending their bitcoin, in exchange for the right to compete to mine Stacks token. Stackers add value to the network and lock up their tokens to earn the BTC that’s being paid by miners to compete to mine STX.\n...';
 
     bot.sendMessage(chatId, faqMessage);
+  }
+  if (data === 'help') {
+    const helpMessage = 'Please join our Discord server and open a ticket for direct support: https://discord.com/invite/velar';
+
+    bot.sendMessage(chatId, helpMessage);
   }
 });
